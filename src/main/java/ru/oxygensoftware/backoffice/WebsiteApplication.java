a1a4415326ccdf999b98a02f57b3437a04da7c40
Dmitry Raguzin <dm.raguzin@jet.msk.su>
2015-11-09 13:52:09 +0300

    war

--- src/main/java/ru/oxygensoftware/backoffice/WebsiteApplication.java
+++ src/main/java/ru/oxygensoftware/backoffice/WebsiteApplication.java
@@ -2,11 +2,17 @@ package ru.oxygensoftware.backoffice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("ru.oxygensoftware.backoffice")
-public class WebsiteApplication {
+public class WebsiteApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebsiteApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(WebsiteApplication.class, args);
     }
